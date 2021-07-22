9290e974ae8744e7545fd942b60c284094e0834b
XMMarlowe <2531649481@qq.com>
2021-07-22 21:10:07 +0800

    update ...

---
+++ service/service_vod/src/test/java/com/marlowe/vodtest/InitObject.java
@@ -0,0 +1,20 @@
+package com.marlowe.vodtest;
+
+import com.aliyun.oss.ClientException;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.profile.DefaultProfile;
+
+/**
+ * @program: onlineEducation
+ * @description:
+ * @author: Marlowe
+ * @create: 2021-07-22 18:20
+ **/
+public class InitObject {
+    public static DefaultAcsClient initVodClient(String accessKeyId, String accessKeySecret) throws ClientException {
+        String regionId = "cn-shanghai";  // 点播服务接入区域
+        DefaultProfile profile = DefaultProfile.getProfile(regionId, accessKeyId, accessKeySecret);
+        DefaultAcsClient client = new DefaultAcsClient(profile);
+        return client;
+    }
+}
---
+++ service/service_vod/src/test/java/com/marlowe/vodtest/TestVod.java
@@ -0,0 +1,81 @@
+package com.marlowe.vodtest;
+
+
+import com.aliyun.vod.upload.impl.UploadVideoImpl;
+import com.aliyun.vod.upload.req.UploadVideoRequest;
+import com.aliyun.vod.upload.resp.UploadVideoResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.vod.model.v20170321.GetPlayInfoRequest;
+import com.aliyuncs.vod.model.v20170321.GetPlayInfoResponse;
+import com.aliyuncs.vod.model.v20170321.GetVideoPlayAuthRequest;
+import com.aliyuncs.vod.model.v20170321.GetVideoPlayAuthResponse;
+
+import java.util.List;
+
+public class TestVod {
+
+    public static void main(String[] args) {
+        String accessKeyId = "LTAI5tD4S6nub68Jp3xrc5WE";
+        String accessKeySecret = "NvO6hjjJwHK0BsU5EmCCPOBxBP7y0L";
+        String title = "hello world";
+        String fileName = "D:\\PersonalFiles\\JavaLearnning\\在线教育--谷粒学院\\项目资料\\1-阿里云上传测试视频\\6 - What If I Want to Move Faster.mp4";
+
+        //上传视频的方法
+        UploadVideoRequest request = new UploadVideoRequest(accessKeyId, accessKeySecret, title, fileName);
+        /* 可指定分片上传时每个分片的大小，默认为2M字节 */
+        request.setPartSize(2 * 1024 * 1024L);
+        /* 可指定分片上传时的并发线程数，默认为1，(注：该配置会占用服务器CPU资源，需根据服务器情况指定）*/
+        request.setTaskNum(1);
+
+        UploadVideoImpl uploader = new UploadVideoImpl();
+        UploadVideoResponse response = uploader.uploadVideo(request);
+
+        if (response.isSuccess()) {
+            System.out.print("VideoId=" + response.getVideoId() + "\n");
+        } else {
+            /* 如果设置回调URL无效，不影响视频上传，可以返回VideoId同时会返回错误码。其他情况上传失败时，VideoId为空，此时需要根据返回错误码分析具体错误原因 */
+            System.out.print("VideoId=" + response.getVideoId() + "\n");
+            System.out.print("ErrorCode=" + response.getCode() + "\n");
+            System.out.print("ErrorMessage=" + response.getMessage() + "\n");
+        }
+    }
+
+    //1 根据视频iD获取视频播放凭证
+    public static void getPlayAuth() throws Exception {
+
+        DefaultAcsClient client = InitObject.initVodClient("LTAI4FvvVEWiTJ3GNJJqJnk7", "9st82dv7EvFk9mTjYO1XXbM632fRbG");
+
+        GetVideoPlayAuthRequest request = new GetVideoPlayAuthRequest();
+        GetVideoPlayAuthResponse response = new GetVideoPlayAuthResponse();
+
+        request.setVideoId("474be24d43ad4f76af344b9f4daaabd1");
+
+        response = client.getAcsResponse(request);
+        System.out.println("playAuth:" + response.getPlayAuth());
+    }
+
+    //1 根据视频iD获取视频播放地址
+    public static void getPlayUrl() throws Exception {
+        //创建初始化对象
+        DefaultAcsClient client = InitObject.initVodClient("LTAI4FvvVEWiTJ3GNJJqJnk7", "9st82dv7EvFk9mTjYO1XXbM632fRbG");
+
+        //创建获取视频地址request和response
+        GetPlayInfoRequest request = new GetPlayInfoRequest();
+        GetPlayInfoResponse response = new GetPlayInfoResponse();
+
+        //向request对象里面设置视频id
+        request.setVideoId("474be24d43ad4f76af344b9f4daaabd1");
+
+        //调用初始化对象里面的方法，传递request，获取数据
+        response = client.getAcsResponse(request);
+
+        List<GetPlayInfoResponse.PlayInfo> playInfoList = response.getPlayInfoList();
+        //播放地址
+        for (GetPlayInfoResponse.PlayInfo playInfo : playInfoList) {
+            System.out.print("PlayInfo.PlayURL = " + playInfo.getPlayURL() + "\n");
+        }
+        //Base信息
+        System.out.print("VideoBase.Title = " + response.getVideoBase().getTitle() + "\n");
+    }
+}
+
